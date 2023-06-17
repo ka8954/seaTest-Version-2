90cda2eaffe61d2eacdd28bfb64125d0f71f6531
Kaarthik Sai Charan Ayineni <93912401+ka8954@users.noreply.github.com>
2023-06-17 18:27:38 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,8 @@
-import mysql.connector
 import streamlit as slt
 import random
 import pandas as pd
 
-conn = slt.experimental_connection('mysql',type='sql')
-
+print("Connection Established")
 
 def main():
     slt.set_page_config(page_title="seaTest - Instructor Interface", page_icon="✨")
@@ -49,14 +47,43 @@ def main():
 
         slt.title('TEST SEATING ARRANGEMENT')
         slt.subheader('DEPT OF NETWORKING AND COMMUNICATIONS')
-        slt.text('This System Allocates Seating for Students belonging to Cloud Computing, Cyber Security, IT, IOT, Networking Specializations')
+        slt.text(
+            'This System Allocates Seating for Students belonging to Cloud Computing, Cyber Security, IT, IOT, Networking Specializations')
         slt.write('# Type Of Exam')
-        radio_btr = slt.selectbox(' ',options=("Choose an One", "Internals", "University Practical", "University Theory"))
+        radio_btr = slt.selectbox(' ',
+                                  options=("Choose an One", "Internals", "University Practical", "University Theory"))
         slt.write('# Enter the Details')
-        df = conn.query('SELECT * FROM seat;', ttl=600)
+        id = slt.text_input('USER ID', max_chars=15)
+        UNAME = slt.text_input('USERNAME', max_chars=6)
+        PWORD = slt.text_input('PASSWORD', max_chars=4)
+        labcapacity = 50
 
-        for row in df.itertuples():
-            slt.write(f"{row.ID} {row.USERNAME} {row.Password} {row.Sysno} {row.Setno} {row.ExamDate}:")
+        a = 1
+        x = [i for i in range(a, labcapacity + 1)]
+        random.shuffle(x)
+
+        for i in range(0, 50):
+            seat = x[i]
+            if seat % 2 == 0:
+                Setno = "SET-1"
+            else:
+                Setno = "SET-2"
+
+        exam_date = slt.date_input("SELECT THE DATE OF EXAM")
+        print(exam_date)
+
+        if slt.button("SUBMIT"):
+            slt.success("Details Saved")
+
+            co1, co2 = slt.columns(2)
+            co3, co4 = slt.columns(2)
+            co5, co6 = slt.columns(2)
+            co1.info('Seat No ')
+            co2.success(seat)
+            co3.info('Set No ')
+            co4.success(Setno)
+            co5.info('Date Of Exam ')
+            co6.success(exam_date)
 
     if options=='Entry':
         entry()
